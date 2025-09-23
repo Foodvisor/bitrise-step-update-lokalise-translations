11126c754e34b6b76ec9bf811ebe4c337a7672c7
louisefoodvisor <121792009+louisefoodvisor@users.noreply.github.com>
2025-09-23 11:37:48 +0200

    Capture and echo lokalise2 version

--- step.sh
+++ step.sh
@@ -3,7 +3,8 @@ set -ex
 
 # For more information, read https://github.com/lokalise/lokalise-cli-2-go/blob/master/docs/lokalise2_file_download.md
 
-echo lokalise2 --version
+version=$(lokalise2 --version)
+echo "$version"
 
 lokalise2 \
     file download \
