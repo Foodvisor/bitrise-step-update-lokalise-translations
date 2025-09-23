029bd7bcb0fb12c0f5e343283945dfc5d3e9f410
louisefoodvisor <121792009+louisefoodvisor@users.noreply.github.com>
2025-09-23 11:13:01 +0200

    Display lokalise2 version in step.sh
    Add command to display lokalise2 version

--- step.sh
+++ step.sh
@@ -3,6 +3,8 @@ set -ex
 
 # For more information, read https://github.com/lokalise/lokalise-cli-2-go/blob/master/docs/lokalise2_file_download.md
 
+echo lokalise2 --version
+
 lokalise2 \
     file download \
     --token ${lokalise_token} \
